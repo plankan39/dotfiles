adc524cb8c95324c2112e09d464ec4068d53dc67
Lukas Elmlund <lukas.elmlund@gmail.com>
2025-01-03 18:16:06 +0100

    refactor

---
+++ fzf/.config/zsh/zshenv.d/fzf.zsh
@@ -0,0 +1,6 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/lukaselmlund/.local/share/fzf/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/lukaselmlund/.local/share/fzf/fzf/bin"
+fi
+
--- zsh/.config/zsh/.zshrc
+++ zsh/.config/zsh/.zshrc
@@ -4,9 +4,8 @@ source $ZDOTDIR/plugins
 # Completions
 source $ZDOTDIR/completions
 
-
+# Extra configurations
 for file in $ZDOTDIR/zshrc.d/*; do 
   source $file; 
 done
-
 
